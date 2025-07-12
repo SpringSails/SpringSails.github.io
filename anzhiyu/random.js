86dedeffd6fd46df6aedd58920fa1635a4058506
SpringSails <3501045856@qq.com>
2025-07-12 16:07:46 +0800

    Site updated: 2025-07-12 16:07:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/11/FirstPage/","2025/07/12/埃氏筛法/","2025/07/12/友元函数无法访问私有成员/","2025/07/12/模板函数声明与定义分离的问题/"];function toRandomPost(){
+var posts=["2025/07/11/FirstPage/","2025/07/12/埃氏筛法/","2025/07/12/模板函数声明与定义分离的问题/","2025/07/12/友元函数无法访问私有成员/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
