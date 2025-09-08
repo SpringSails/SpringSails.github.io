513990e013684c7b02cf6db86435a8b131deff3f
SpringSails <3501045856@qq.com>
2025-09-08 11:41:00 +0800

    Site updated: 2025-09-08 11:41:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/11/FirstPage/","2025/07/12/explicit关键字导致的没有适当的类复制构造函数问题/","2025/07/12/友元函数无法访问私有成员/","2025/07/12/埃氏筛法/","2025/07/12/模板函数声明与定义分离的问题/"];function toRandomPost(){
+var posts=["2025/09/08/DFS和BFS学习笔记/","2025/07/11/FirstPage/","2025/07/12/友元函数无法访问私有成员/","2025/07/12/explicit关键字导致的没有适当的类复制构造函数问题/","2025/07/12/模板函数声明与定义分离的问题/","2025/07/12/埃氏筛法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
